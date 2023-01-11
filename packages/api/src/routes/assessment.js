2d136f596951b346386adcf6bfcab78afd6d693e
Dikshya Thapa <thapada@mail.uc.edu>
2023-01-11 16:34:53 -0500

    Added path /list and awaiting till data received

--- packages/api/src/routes/assessment.js
+++ packages/api/src/routes/assessment.js
@@ -28,13 +28,13 @@ assessmentRouter.post(
 );
 
 assessmentRouter.get(
-  `/`,
-  // eslint-disable-next-line require-await
+  `/list`,
+
   async (req, res, next) => {
     try {
       // verify that your data is making it here to the API by using console.log();
       // call the AssessmentService.getList function from packages/api/src/microservices/Assessment-Service.js
-      const assessments = [];
+      const assessments = await AssessmentService.getList();
 
       ResponseHandler(
         res,
